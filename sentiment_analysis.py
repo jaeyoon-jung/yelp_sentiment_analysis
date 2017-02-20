126c01f74528e48d290259513dc64e480c4eaf58
Jaeyoon Jung <jaeyoonjung@Jaeyoons-MacBook-Pro.local>
2017-02-21 00:12:31 +0900

    bigram analysis added

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -4,9 +4,15 @@ import nltk.classify.util
 from nltk.classify import NaiveBayesClassifier
 from nltk.corpus import stopwords
 import nltk.classify.util
+from nltk.stem.porter import PorterStemmer
 import math
 from collections import Counter
 import pickle
+import collections
+import itertools
+import nltk
+from nltk.collocations import BigramCollocationFinder
+from nltk.metrics import BigramAssocMeasures
 
 
 def format(review):
@@ -44,36 +50,12 @@ def extract_feature(text, blacklist):
 
     return feat
 
-
-# read lines between start and end and classify its sentiment
-def save_SA_results(yelp_f, output_f, start, end, model, blacklist):
-    fp = open(yelp_f)
-    for i, line in enumerate(fp):
-        if i in range(start, end):
-            review = json.loads(line)
-            with open(output_f, 'a') as outfile:
-                feat = extract_feature(review['text'], blacklist)
-                sentiment_dist = model.prob_classify(feat)
-
-                sentiment = model.classify(feat)
-                pos_prob = sentiment_dist.prob('pos')
-                neg_prob = sentiment_dist.prob('neg')
-
-                stars = review['stars']
-
-                data = {}
-                data['date'] = review['date']
-                data['text'] = review['text']
-                data['votes'] = review['votes']
-                data['stars'] = review['stars']
-                data['sentiment'] = sentiment
-                data['positive_probability'] = pos_prob
-                data['negative_probability'] = neg_prob
-
-                out = json.dumps(data)
-                outfile.write(out + '\n')
-    print ('Wrote ' + output_f)
-
+def bigram_word_feats(words, significant_uni, score_fn=BigramAssocMeasures.chi_sq, n=20):
+    stoplist = set(stopwords.words("english"))
+    bigram_finder = BigramCollocationFinder.from_words(words)
+    bigrams = bigram_finder.nbest(score_fn, n)
+    return dict([(ngram, True) for ngram in itertools.chain(words, bigrams) if ngram not in significant_uni and
+                ngram not in stoplist])
 
 # construct a training dataset and train a model on it
 def main():
@@ -82,69 +64,68 @@ def main():
     negatives = []
 
     yelp_f = 'yelp_dataset_challenge_academic_dataset/yelp_academic_dataset_review.json'
-    start = 0
-    end = 250000
-    fp = open(yelp_f)
-    for i, line in enumerate(fp):
-        if i in range(start, end):
-            review = json.loads(line)
-            review_text = review['text']
-
-            if review['stars'] == 5:
-                positives.append(review_text)
-            elif review['stars'] == 1:
-                negatives.append(review_text)
-
-    positives = list(set(positives))
-    negatives = list(set(negatives))
-
-    print ('Positive training data: ' + str(len(positives)) + " reviews")
-    print ('Negative training data: ' + str(len(negatives)) + " reviews")
+    f = open(yelp_f, 'r')
+    for i in range(0, 500000):
+        review = json.loads(f.readline())
+        review_text = review['text']
+        if review['stars'] == 5:
+            positives.append(review_text)
+        elif review['stars'] == 1:
+            negatives.append(review_text)
+    
+    print ('Collected %d positive reviews' %len(positives))
+    print ('Collected %d negative reviews' %len(negatives))
 
     # stoplist from nltk
     stoplist = set(stopwords.words("english"))
 
+    # lists to store preprocessed text
     posreviews = []
+    negreviews = []
+
+    # remove stopword and extract stemword
     for positive in positives:
         formatted_positive = format(positive)
-        pos = [word for word in formatted_positive.lower().split() if word not in
+        pos = [PorterStemmer().stem(word).encode("utf-8") for word in formatted_positive.lower().split() if word not in
                stoplist and len(word) > 1]
         posreviews.append(pos)
 
-    negreviews = []
     for negative in negatives:
         formatted_negative = format(negative)
-        neg = [word for word in formatted_negative.lower().split() if word not in
+        neg = [PorterStemmer().stem(word.encode("utf-8")) for word in formatted_negative.lower().split() if word not in
                stoplist and len(word) > 1]
         negreviews.append(neg)
 
     # identify words that appear too often in both positive and negative reviews
     # remove them since they are probably not very informative
-    poswords = [item for sublist in posreviews for item in sublist]
+    poswords = [word for wordlist in posreviews for word in wordlist]
     poscnt = Counter()
     for posword in poswords:
         poscnt[posword] += 1
 
     top1000_pos = poscnt.most_common(1000)
 
-    negwords = [item for sublist in negreviews for item in sublist]
+    negwords = [word for wordlist in negreviews for word in wordlist]
     negcnt = Counter()
     for negword in negwords:
         negcnt[negword] += 1
 
     top1000_neg = negcnt.most_common(1000)
 
-    zipf_pos = []
-    zipf_neg = []
+    freq_pos = []
+    freq_neg = []
+
+    # top1000_pos and top1000_neg are dictionary pairs of word and number of appearance.
+    # Extract just the words (key)
     for pos_dict in top1000_pos:
         pos_key = pos_dict[0]
-        zipf_pos.append(pos_key)
+        freq_pos.append(pos_key)
     for neg_dict in top1000_neg:
         neg_key = neg_dict[0]
-        zipf_neg.append(neg_key)
+        freq_neg.append(neg_key)
 
-    # application of zipfian theory- words of top1000 frequency shold be good enough
-    toocommon = [val for val in zipf_pos if val in zipf_neg]
+    # application of zipfian theory- words of top100 frequency shold be good enough
+    toocommon = [common for common in freq_pos if common in freq_neg]
 
     for i in range(0, len(posreviews)):
         posreviews[i] = [word for word in posreviews[i] if word not in toocommon and len(word) > 1]
@@ -156,8 +137,8 @@ def main():
     negfeats = [(word_feats(review), 'neg') for review in negreviews]
     posfeats = [(word_feats(review), 'pos') for review in posreviews]
 
-    poscutoff = math.ceil(len(posfeats) * 0.8)
-    negcutoff = math.ceil(len(negfeats) * 0.8)
+    poscutoff = int(len(posfeats) * 0.8)
+    negcutoff = int(len(negfeats) * 0.8)
     pos_n = len(posfeats)
     neg_n = len(negfeats)
 
@@ -170,16 +151,81 @@ def main():
     naive_classifier = NaiveBayesClassifier.train(trainfeats)
     naive_classifier.show_most_informative_features()
 
-    # 83.9% accuracy. not bad!
     testfeats = testneg + testpos
-    print ('Model Accuracy:', nltk.classify.util.accuracy(naive_classifier, testfeats))
+
+    refsets = collections.defaultdict(set)
+    testsets = collections.defaultdict(set)
+
+    for i, (feats, label) in enumerate(testfeats):
+        refsets[label].add(i)
+        predicted = naive_classifier.classify(feats)
+        testsets[predicted].add(i)
+
+    # 86.7% accuracy.
+    print ('accuracy:', nltk.classify.util.accuracy(naive_classifier, testfeats))
+    print ('pos precision:', nltk.precision(refsets['pos'], testsets['pos']))
+    print ('pos recall:', nltk.recall(refsets['pos'], testsets['pos']))
+    print ('neg precision:', nltk.precision(refsets['neg'], testsets['neg']))
+    print ('neg recall:', nltk.recall(refsets['neg'], testsets['neg']))
+
+    # save unigram classifier
+    pickle.dump(naive_classifier, open("unigram_classifier.p", "wb"))
+
+    # now bigram analysis!
+    # lists to store preprocessed text
+    bi_posreviews = []
+    bi_negreviews = []
+
+    # remove stopword and get extract stemword
+    for positive in positives:
+        formatted_positive = format(positive)
+        pos = [PorterStemmer().stem(word) for word in formatted_positive.lower().split() if len(word) > 1]
+        bi_posreviews.append(pos)
+
+    for negative in negatives:
+        formatted_negative = format(negative)
+        neg = [PorterStemmer().stem(word) for word in formatted_negative.lower().split() if len(word) > 1]
+        bi_negreviews.append(neg)
+
+    # extract features
+    bi_negfeats = [(bigram_word_feats(words=review, significant_uni=toocommon, n=20), 'neg') for review in bi_negreviews
+                   if len(set(review)) > 1]
+    bi_posfeats = [(bigram_word_feats(words=review, significant_uni=toocommon, n=20), 'pos') for review in bi_posreviews
+                   if len(set(review)) > 1]
+
+    poscutoff = int(len(bi_posfeats) * 0.8)
+    negcutoff = int(len(bi_negfeats) * 0.8)
+    pos_n = len(bi_posfeats)
+    neg_n = len(bi_negfeats)
+
+    # build a naive bayes classifier
+    bi_trainpos = bi_posfeats[0:poscutoff]
+    bi_trainneg = bi_negfeats[0:negcutoff]
+    bi_testpos = bi_posfeats[poscutoff:pos_n]
+    bi_testneg = bi_negfeats[negcutoff:neg_n]
+    bi_trainfeats = bi_trainneg + bi_trainpos
+    bigram_naive_classifier = NaiveBayesClassifier.train(bi_trainfeats)
+    bigram_naive_classifier.show_most_informative_features()
+
+    # 83.9% accuracy. not bad!
+    bi_testfeats = bi_testneg + bi_testpos
+    bi_refsets = collections.defaultdict(set)
+    bi_testsets = collections.defaultdict(set)
+
+    for i, (feats, label) in enumerate(bi_testfeats):
+        bi_refsets[label].add(i)
+        bi_predicted = bigram_naive_classifier.classify(feats)
+        bi_testsets[bi_predicted].add(i)
+
+    # 90.8% accuracy
+    print ('accuracy:', nltk.classify.util.accuracy(bigram_naive_classifier, bi_testfeats))
+    print ('pos precision:', nltk.precision(bi_refsets['pos'], bi_testsets['pos']))
+    print ('pos recall:', nltk.recall(bi_refsets['pos'], bi_testsets['pos']))
+    print ('neg precision:', nltk.precision(bi_refsets['neg'], bi_testsets['neg']))
+    print ('neg recall:', nltk.recall(bi_refsets['neg'], bi_testsets['neg']))
 
     # save the model in a pickle file
-    pickle.dump(naive_classifier, open("classifier.p", "wb"))
+    pickle.dump(bigram_naive_classifier, open("bigram_classifier.p", "wb"))
 
-    # divide the original dataset into 10 chunks, save the result fo each
-    for i in range(0, 10):
-        result_f = 'naive_result/result' + str(i) + '.json'
-        start = 0 + (i * 268506)
-        end = 268506 * (i + 1)
-        save_SA_results(yelp_f, result_f, start, end, naive_classifier, toocommon)
+    # save frequent word list
+    pickle.dump(toocommon, open("uninformative.p", 'wb'))
